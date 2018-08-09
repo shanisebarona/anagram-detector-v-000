3ac9d1a41dd4b73723858d2d90d386be86ad9902
Shanise Barona <shanisebarona@gmail.com>
2018-08-09 00:36:32 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.each do |w| w == @word
+    # binding.pry 
+    end
+  end
+  
+  
+  
+  #given a word AND list of anagrams
+  
+  
+  
+  
+  
+  
+  
+  
+end
